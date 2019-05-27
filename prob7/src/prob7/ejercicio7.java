10a92f615896aed32ac8842e4f67079f930200fa
Yamil <Yamil@192.168.1.5>
2019-05-27 17:17:33 -0500

    problema7

---
+++ prob7/src/prob7/ejercicio7.java
@@ -0,0 +1,30 @@
+package prob7;
+
+public class ejercicio7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,x=4  ;
+		int w=9 , q ;
+		for(i=-1;i<20 ; i+=3) {
+			x++ ;
+			for(q=4;q<11;q++) {
+				do {
+				i=+3 ;
+						w=4 ;
+						i=x+w ;
+						x=w+i ;
+			}while(x<15);
+			
+				
+				
+			}
+		}
+              System.out.println("x: "+x+" i: "+i) ;
+
+}
+}
+
+	
+
+
